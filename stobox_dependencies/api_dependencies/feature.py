05fc5d183f0c13437c8d5fe099e1ea2e73785721
SerhiiLakodei <>
2025-04-17 16:45:26 +0300

    update FeatureEnabled depends

--- stobox_dependencies/api_dependencies/feature.py
+++ stobox_dependencies/api_dependencies/feature.py
@@ -8,14 +8,15 @@ from stobox_dependencies.clients import payment_client
 from stobox_dependencies.settings.constants import ErrorMessages
 
 
-async def feature_enabled(
-    feature: str,
-    user_id: int = Depends(current_user_id),
-) -> None:
-    response = await payment_client.is_feature_enables(user_id, feature)
-    if not response.get('is_enabled', False):
-        raise HTTPException(
-            status_code=HTTPStatus.FORBIDDEN,
-            detail=ErrorMessages.FEATURE_NOT_ENABLED.format(feature=feature),
-            headers={'WWW-Authenticate': 'Bearer'},
-        )
+class FeatureEnabled:
+    def __init__(self, feature: str):
+        self.feature = feature
+
+    async def __call__(self, user_id: int = Depends(current_user_id)):
+        response = await payment_client.is_feature_enables(user_id, self.feature)
+        if not response.get('is_enabled', False):
+            raise HTTPException(
+                status_code=HTTPStatus.FORBIDDEN,
+                detail=ErrorMessages.FEATURE_NOT_ENABLED.format(feature=self.feature),
+                headers={'WWW-Authenticate': 'Bearer'},
+            )
