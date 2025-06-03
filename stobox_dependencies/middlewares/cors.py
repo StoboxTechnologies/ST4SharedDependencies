f3c5f3e0e526eb1fd0b2da663f1b2f62353a4c65
SerhiiLakodei <>
2025-06-03 14:54:09 +0300

    update middleware

--- stobox_dependencies/middlewares/cors.py
+++ stobox_dependencies/middlewares/cors.py
@@ -1,15 +1,20 @@
 import boto3
-from starlette.middleware.cors import ALL_METHODS
 from starlette.middleware.cors import CORSMiddleware
+from starlette.types import Receive
+from starlette.types import Scope
+from starlette.types import Send
 
 from stobox_dependencies.settings.conf import settings
 
 
 class DynamicCORSMiddleware(CORSMiddleware):
     def __init__(self, *args, **kwargs):
+        self.dynamo_table = self.get_dynamodb_table()
         super().__init__(*args, **kwargs)
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         self.allow_origins = self.get_allowed_origins()
-        self.allow_methods = kwargs.get('allow_methods', ALL_METHODS)
+        await super().__call__(scope, receive, send)
 
     @staticmethod
     def get_dynamodb_table():
@@ -21,15 +26,11 @@ class DynamicCORSMiddleware(CORSMiddleware):
         dynamodb = session.resource('dynamodb')
         return dynamodb.Table(settings.ALLOWED_ORIGINS_TABLE_NAME)
 
-    @classmethod
-    def get_allowed_origins(cls) -> list[str]:
-        table = cls.get_dynamodb_table()
-        scan_response = table.scan(AttributesToGet=['origin'])
+    def get_allowed_origins(self) -> list[str]:
+        scan_response = self.dynamo_table.scan(AttributesToGet=['origin'])
         return [item['origin'] for item in scan_response.get('Items', [])]
 
-    @classmethod
-    def set_allowed_origins(cls, allowed_origins: list[str]):
-        table = cls.get_dynamodb_table()
-        with table.batch_writer() as batch:
+    def set_allowed_origins(self, allowed_origins: list[str]):
+        with self.dynamo_table.batch_writer() as batch:
             for origin in allowed_origins:
                 batch.put_item(Item={'origin': origin})
