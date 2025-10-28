e0d1e6256aa5ebfa3e9213e39b8f1aa9b1137be4
jenjamc <jenjamak@gmail.com>
2025-10-28 13:17:22 +0200

    pre commit

--- stobox_dependencies/clients/company.py
+++ stobox_dependencies/clients/company.py
@@ -1,5 +1,6 @@
 from stobox_dependencies.clients.http import BaseHTTPClient
 from stobox_dependencies.schemes.company import Company
+from stobox_dependencies.schemes.company import TokenSchema
 
 
 class CompanyHTTPClient(BaseHTTPClient):
@@ -10,7 +11,6 @@ class CompanyHTTPClient(BaseHTTPClient):
     async def get_public_company_info(self, company_id: int) -> Company:
         response = await self.get(url=self.ROUTES.PUBLIC_COMPANY_INFO.format(company_id=company_id))
         return Company.model_validate(response.json())
-
 
     async def get_token_info_by_address(self, address: str) -> TokenSchema:
         response = await self.get(url=self.ROUTES.TOKEN_INFO_BY_ADDRESS.format(address=address))
