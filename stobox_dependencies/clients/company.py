91140daf52c58a712781fa66cd8957a89c4b7c59
jenjamc <jenjamak@gmail.com>
2025-10-28 13:56:28 +0200

    fix url

--- stobox_dependencies/clients/company.py
+++ stobox_dependencies/clients/company.py
@@ -6,7 +6,7 @@ from stobox_dependencies.schemes.company import TokenSchema
 class CompanyHTTPClient(BaseHTTPClient):
     class ROUTES:
         PUBLIC_COMPANY_INFO: str = '/companies/{company_id}/public'
-        TOKEN_INFO_BY_ADDRESS: str = 'internal/tokens/by-address/{address}'
+        TOKEN_INFO_BY_ADDRESS: str = 'internal/companies/tokens/by-address/{address}'
 
     async def get_public_company_info(self, company_id: int) -> Company:
         response = await self.get(url=self.ROUTES.PUBLIC_COMPANY_INFO.format(company_id=company_id))
